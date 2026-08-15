7f934944ae0f3e740a1a859d1c69359d0ca1556e
Cyberdyne Systems T-800 <51139116+LakshendraSingh@users.noreply.github.com>
2026-08-15 16:00:08 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        for (int i=n-1; i>=0; i--){
+            for (int j=0; j<n; j++){
+                if (i == j)
+                    continue;
+                else if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+};

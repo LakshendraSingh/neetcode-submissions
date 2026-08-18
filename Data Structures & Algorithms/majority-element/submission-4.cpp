b2d92abd510d69ab375a040dbc15db049013fb12
Cyberdyne Systems T-800 <51139116+LakshendraSingh@users.noreply.github.com>
2026-08-18 15:34:20 +0530

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int nums_size=nums.size(), maj = (nums_size/2);
+        unordered_map <int, int> mp;
+        for(auto x:nums)
+            mp[x] += 1; 
+        for(auto x:mp)
+            if(x.second > maj) 
+                return x.first;
+        return 0;
+    }
+};

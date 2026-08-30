6fee081112bcd93c03355de3dbf4e84b741b30b5
Cyberdyne Systems T-800 <51139116+LakshendraSingh@users.noreply.github.com>
2026-08-30 19:00:49 +0530

    Add: range-sum-query-2d-immutable - submission-3

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-3.cpp
@@ -0,0 +1,32 @@
+class NumMatrix {
+private:
+    vector <vector<int>> mat, prefix_sum;
+public:    
+    NumMatrix(vector<vector<int>>& matrix) {
+        mat=matrix;
+        int rs_mat=mat.size(), cs_mat=mat[0].size();
+        prefix_sum=vector<vector<int>>(rs_mat+1,vector<int>(cs_mat+1,0));
+
+        for(auto i=1;i<=rs_mat;i++){
+            for(auto j=1;j<=cs_mat;j++){
+                prefix_sum[i][j]=mat[i-1][j-1]
+                                +prefix_sum[i-1][j]
+                                +prefix_sum[i][j-1]
+                                -prefix_sum[i-1][j-1];
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return prefix_sum[row2+1][col2+1]
+                -prefix_sum[row2+1][col1]
+                -prefix_sum[row1][col2+1]
+                +prefix_sum[row1][col1];
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
